504b3e9c429208837dc36204e08c6c283a2f0ea2
Marica Antonacci <marica.antonacci@ba.infn.it>
2020-04-05 19:01:49 +0200

    Add infn-cloud profile

---
+++ config/infn-cloud.py
@@ -0,0 +1,7 @@
+#### LOOK AND FEEL SETTINGS
+WELCOME_MESSAGE = "Welcome to the INFN Cloud Dashboard!"
+NAVBAR_BRAND_TEXT = "INFN Cloud Dashborad"
+
+### Template Paths
+HOME_TEMPLATE = 'infn-cloud/home.html'
+FOOTER_TEMPLATE = 'infn-cloud/footer.html'
